38af64b930d0f79a87562afaffd79b4bb9776dd5
Aniket <01aniket26@gmail.com>
2022-10-23 16:01:52 +0530

    all backend files

---
+++ util/location.js
@@ -0,0 +1,21 @@
+const axios = require("axios");
+const HttpError = require('../models/http-error');
+const API_KEY = "AIzaSyCc8B2-TYtLr7-ZpB_JhE1bWp-vsgXY4Kw";
+
+const getCoordsforAddress = async (address) => {
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${API_KEY}`
+  );
+  const data = await response.data;
+  if(!data || data.status==='ZERO_RESULTS')
+  {
+    const error = new HttpError('could not find location for the specified address.',422);
+    throw error;
+  }
+  const coordinates = await data.results[0].geometry.location;
+  return coordinates;
+};
+
+module.exports = getCoordsforAddress;
